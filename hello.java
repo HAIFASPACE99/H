f2c6f2ae355f048b40fb2b7c37dbb3f02448297b
HAIFASPACE99 <blackkspace1999@gmail.com>
2023-05-02 05:24:24 +0300

    hw--1

--- hello.java
+++ hello.java
@@ -4,9 +4,9 @@ public class hello {
    
     public static void main(String[] args) {
 
-            System.out.println("  hello world");
+            System.out.println("  hello world  *  ");
 
-        //nnn
+        
         
     }
 
